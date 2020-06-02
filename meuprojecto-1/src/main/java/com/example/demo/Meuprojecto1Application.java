61fc0e89438f436a9c525bde3ba5c6a1a704fd0f
gmanuel <gmanuel@ZAMPTCONGMANUEL.mshome.net>
2020-06-02 17:47:04 +0200

    primeira versao

---
+++ meuprojecto-1/src/main/java/com/example/demo/Meuprojecto1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Meuprojecto1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Meuprojecto1Application.class, args);
+	}
+
+}
